530713377f313e551ce783ee4cf39f8b0b4dad22
EmCousin <emmanuel_cousin@hotmail.fr>
2018-08-08 19:51:48 +0200

    add pdf generation

---
+++ app/controllers/pdfs_controller.rb
@@ -0,0 +1,46 @@
+class PdfsController < ApplicationController
+  AVATAR_URL = "https://media.licdn.com/dms/image/C4D03AQFrwBey7t9h5w/profile-displayphoto-shrink_200_200/0?e=1539216000&v=beta&t=ZNMpq2sTW9zR3nBXV9KcK7hqq1cfCScFTGwswFKC_YU"
+
+  def index
+    pdf = WickedPdf.new.pdf_from_string(
+      render_to_string 'templates/pdf.html.erb', layout: 'pdf.html.erb',
+                                                 encoding: 'UTF-8',
+                                                 locals: {
+                                                   first_name: params[:firstName],
+                                                   last_name: params[:lastName] || '',
+                                                   avatar_url: params[:avatarUrl] || AVATAR_URL,
+                                                   current_year: params[:currentYear],
+                                                   next_year: params[:nextYear],
+                                                   courses: params[:courses],
+                                                   date_of_birth: params[:dateOfBirth],
+                                                   citizenship: params[:citizenship],
+                                                   address: params[:address],
+                                                   zip_code: params[:zipCode],
+                                                   city: params[:city],
+                                                   country: params[:country],
+                                                   phone_number: params[:phoneNumber],
+                                                   cellphone_number: params[:cellphoneNumber],
+                                                   email: params[:email],
+                                                   contact_person_name: params[:contactPersonName],
+                                                   contact_relationship: params[:contactRelationship],
+                                                   contact_phone_number: params[:contactPhoneNumber],
+                                                   agreed_to_publicity_right: params[:agreedToPublicityRight],
+                                                   fee: params[:fee]
+                                                 }
+
+    )
+
+    save_path = Rails.root.join('pdfs','filename.pdf')
+    file = Tempfile.open(['fiche', '.pdf']) do |f|
+      f <<  pdf.force_encoding('UTF-8')
+    end
+
+    respond_to do |format|
+      format.html do
+        send_file file.path, type: 'application/pdf',
+                             disposition: 'attachment'
+      end
+    end
+
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,14 @@ module Pkp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.middleware.insert_before 0, Rack::Cors do
+      unless Rails.env.test?
+        allow do
+          origins '*'
+          resource '*', headers: :any, methods: %I[get]
+        end
+      end
+    end
   end
 end
