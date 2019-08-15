3c55bc9fe6e5d7ad5e80ad3d1fc22bd0295bc0fa
Daniele Angeli <daniangelisilva@gmail.com>
2019-08-15 17:29:25 +0200

    translate errors + logout user not admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,16 @@
 class AdminController < ApplicationController
   before_action :authenticate_user!
+  before_action :ensure_current_user_is_admin!
 
   def index
+  end
+
+  private
+
+  def ensure_current_user_is_admin!
+    unless current_user.admin
+      sign_out current_user
+      redirect_to after_sign_out_path_for(:user)
+    end
   end
 end
