4015e41ed0ff0ad8c6252a9cedb0233e42634a35
Daniele Angeli <daniangelisilva@gmail.com>
2019-09-16 16:05:55 +0200

    add actions to the admin users controller

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,47 @@
+class Admin::UsersController < AdminController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users_path, notice: 'Membre créé avec succès !'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @course = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admin_users_path, notice: 'Membre modifié avec succès !'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path, notice: 'Membre supprimé avec succès !'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :phone_number, :address, :zip_code, :city, :country, :contact_name, :contact_phone_number, :contact_relationship, :agreed_to_publicity_right, :avatar)
+  end
+end
