5be96144a99cd37b56b2ca6bd18030c21f50180e
Dani <daniangelisilva@gmail.com>
2020-01-03 21:55:53 +0100

    add missing fields to subscription form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :birthdate, :phone_number, :address, :zip_code, :city, :country, :contact_name, :contact_phone_number, :contact_relationship, :agreed_to_publicity_right, :avatar])
   end
 end
