b5cbba1c7d6ed43745a4b3de4d466b3a72da98d1
EmCousin <emmanuel@braidio.com>
2020-02-04 22:02:18 -0300

    upgrade to rails 6

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,11 @@ Bundler.require(*Rails.groups)
 module Pkp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 6.0
     config.i18n.default_locale = :fr
+
+    config.add_autoload_paths_to_load_path = false
+    config.active_storage.replace_on_assign_to_many = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
