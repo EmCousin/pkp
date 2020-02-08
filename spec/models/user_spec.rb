30b6a99ff20a11b0d17a7acfc228945e59e02bf4
EmCousin <emmanuel@braidio.com>
2020-02-08 13:45:56 -0300

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 describe User, type: :model do
   let(:user) { create :user }
+  let(:too_old_user) { build :user, birthdate: 100.years.ago }
+  let(:too_young_user) { build :user, birthdate: 5.years.ago }
 
   it { is_expected.to have_many(:subscriptions).dependent(:destroy).with_foreign_key(:member_id) }
   it { is_expected.to have_many(:courses).through(:subscriptions) }
@@ -17,8 +19,19 @@ describe User, type: :model do
   it { is_expected.to validate_presence_of(:contact_name) }
   it { is_expected.to validate_presence_of(:contact_phone_number) }
   it { is_expected.to validate_presence_of(:contact_relationship) }
-  it {is_expected.to validate_inclusion_of(:contact_relationship).in_array(described_class::CONTACTS) }
+  it { is_expected.to validate_inclusion_of(:contact_relationship).in_array(described_class::CONTACTS) }
   it { is_expected.to validate_presence_of(:agreed_to_publicity_right) }
+
+  it 'is expected to validate the birthdate range' do
+    expect(user).to be_valid
+    expect(user.errors[:birthdate]).to be_empty
+
+    expect(too_old_user).to be_invalid
+    expect(too_old_user.errors[:birthdate]).not_to be_empty
+
+    expect(too_young_user).to be_invalid
+    expect(too_young_user.errors[:birthdate]).not_to be_empty
+  end
 
   describe 'constants' do
     it 'has a constant CONTACTS' do
