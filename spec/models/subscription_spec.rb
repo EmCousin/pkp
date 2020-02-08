0f3691a618dc56fcea1e540da5f188142a103e3f
EmCousin <emmanuel@braidio.com>
2020-02-08 14:48:12 -0300

    Create subscription_spec.rb

---
+++ spec/models/subscription_spec.rb
@@ -0,0 +1,75 @@
+require 'rails_helper'
+
+describe Subscription, type: :model do
+  let(:category) { Course::CATEGORIES.sample }
+    let(:courses) do
+      [
+        build(:course, category: category, weekday: Course.weekdays.keys.first),
+        build(:course, category: category, weekday: Course.weekdays.keys.last)
+      ]
+    end
+  let(:subscription) { build :subscription, courses: courses }
+
+  it { is_expected.to belong_to(:member).class_name('User') }
+  it { is_expected.to have_many(:courses_subscriptions).dependent(:destroy) }
+  it { is_expected.to have_many(:courses).through(:courses_subscriptions) }
+
+  it { is_expected.to validate_presence_of(:year) }
+  it { is_expected.to validate_presence_of(:fee) }
+  it { is_expected.to validate_numericality_of(:fee).is_greater_than_or_equal_to(0) }
+
+  describe 'validating the year is the current year on creation' do
+    it 'should invalidate a subscription in the future' do
+      subscription.year = Time.now.year + 1
+      expect(subscription).to be_invalid
+      expect(subscription.errors[:year]).not_to be_empty
+    end
+
+    it 'should invalidate a subscription in the past' do
+      subscription.year = Time.now.year - 1
+      expect(subscription).to be_invalid
+      expect(subscription.errors[:year]).not_to be_empty
+    end
+
+    it 'should validate a subscription in the present' do
+      expect(subscription).to be_valid
+    end
+  end
+
+  describe 'validating courses' do
+    before { subscription.valid? }
+
+    it { expect(subscription).to be_valid }
+
+    describe 'validating the subscription has at least one course' do
+      context 'when the subscription has no courses' do
+        let(:courses) { [] }
+
+        it { expect(subscription.errors.details[:courses]).to include({ error: :must_exist }) }
+      end
+
+      context 'when the subscription has more than three courses' do
+        let(:courses) { build_list :course, 4 }
+
+        it { expect(subscription.errors.details[:courses]).to include({ error: :limit_exceeded }) }
+      end
+
+      context 'when the subscription has courses from different categories' do
+        let(:courses) do
+          [
+            build(:course, category: Course::CATEGORIES.first),
+            build(:course, category: Course::CATEGORIES.last),
+          ]
+        end
+
+        it { expect(subscription.errors.details[:courses]).to include({ error: :unique_category }) }
+      end
+
+      context 'when the subscription has multiple courses the same day' do
+        let(:courses) { build_list :course, 2, weekday: Course.weekdays.keys.first }
+
+        it { expect(subscription.errors.details[:courses]).to include({ error: :unique_weekday }) }
+      end
+    end
+  end
+end
