ceccfe27d3cf916acffea8f66c934d34a7fd871e
EmCousin <emmanuel@braidio.com>
2020-04-14 18:38:14 +0200

    rubocop

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -1,8 +1,20 @@
+# frozen_string_literal: true
+
 class SubscriptionMailer < ApplicationMailer
   def confirm_subscription(subscription)
     @subscription = subscription
 
-    pdf = WickedPdf.new.pdf_from_string(
+    pdf = pdf_from_subscription(subscription)
+
+    attachments['fiche.pdf'] = { mime_type: 'application/pdf', content: pdf.force_encoding('UTF-8') }
+
+    mail to: subscription.member.email, subject: "Inscription Parkour Paris #{subscription.year} / #{subscription.year + 1}"
+  end
+
+  private
+
+  def pdf_from_subscription(subscription)
+    WickedPdf.new.pdf_from_string(
       render_to_string(
         'templates/subscription.html.erb',
         layout: 'pdf.html.erb',
@@ -12,9 +24,5 @@ class SubscriptionMailer < ApplicationMailer
         }
       )
     )
-
-    attachments['fiche.pdf'] = { mime_type: 'application/pdf', content: pdf.force_encoding('UTF-8') }
-
-    mail to: subscription.member.email, subject: "Inscription Parkour Paris #{subscription.year} / #{subscription.year + 1}"
   end
 end
