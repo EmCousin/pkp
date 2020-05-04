5a8fe6f6eb5d2ec5a00b0bab23c6cda876fbf2b8
Dani <daniangelisilva@gmail.com>
2020-05-04 18:31:04 +0200

    add method to restrict subscriptions during vacation time + generate new controller and related view and route

---
+++ app/controllers/dashboard/vacations_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Dashboard
+  class VacationsController < DashboardController
+    def index; end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,7 @@ Rails.application.routes.draw do
       resource :signed_form, only: [:edit, :update]
       resource :payment, only: [:new, :create]
     end
+    resources :vacations, only: [:index]
   end
 
   resources :dashboard, only: [:index]
