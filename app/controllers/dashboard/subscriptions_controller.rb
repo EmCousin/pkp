595e8bbe005ed20645ae8cb78073a5046e0149c2
Dani <daniangelisilva@gmail.com>
2020-05-06 17:47:18 +0200

    add method, controller and view to impair subscriptions when there is no more course places

--- app/controllers/dashboard/subscriptions_controller.rb
+++ app/controllers/dashboard/subscriptions_controller.rb
@@ -3,6 +3,7 @@
 module Dashboard
   class SubscriptionsController < DashboardController
     before_action :check_vacation_time!, only: %i[new create]
+    before_action :check_availability!, only: %i[new create]
 
     def new
       @form = CreateSubscriptionForm.new(subscription_params)
@@ -32,6 +33,18 @@ module Dashboard
       return unless [7, 8].include?(Time.now.month)
 
       redirect_to dashboard_vacations_path
+    end
+
+    def check_availability!
+      course_available = false
+      Course.all.each do |course|
+        active_subscriptions = course.subscriptions.where(year: Time.now.year).where.not(status: 'archived')
+        count = active_subscriptions.count
+        course_available = true if course.capacity > count
+      end
+      return if course_available
+
+      redirect_to dashboard_capacities_path
     end
   end
 end
