cbd4f3039167a1b0ccebac67091b6a59ad97da6c
EmCousin <emmanuel@braidio.com>
2020-06-02 09:55:21 +0200

    add production config for action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,11 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "pkp_#{Rails.env}"
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.default_url_options = {
+    host: Rails.application.credentials.url_options[:host],
+    protocol: 'https'
+  }
+a
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
