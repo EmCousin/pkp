87263ec9ed072c3a1426025f7f9a494a04fe71c9
EmCousin <emmanuel@braidio.com>
2020-06-02 11:23:30 +0200

    remove useless upload fields since ActiveStorage uses a dedicated table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_02_160928) do
+ActiveRecord::Schema.define(version: 2020_06_02_092126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,8 +71,6 @@ ActiveRecord::Schema.define(version: 2020_05_02_160928) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "status", default: 0
-    t.string "medical_certificate"
-    t.string "signed_form"
     t.string "stripe_charge_id"
     t.index ["member_id"], name: "index_subscriptions_on_member_id"
   end
