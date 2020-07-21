32fdbefc9e1ba40ce805be149bf70dacb53b2dd8
EmCousin <emmanuel@braidio.com>
2020-07-21 18:58:42 +0200

    update domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,11 +77,11 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
   config.action_mailer.smtp_settings = {
-      port:            Rails.application.credentials.mailgun[:smtp_port],
-      address:         Rails.application.credentials.mailgun[:smtp_server],
-      user_name:       Rails.application.credentials.mailgun[:smtp_login],
-      password:        Rails.application.credentials.mailgun[:smtp_password],
-      domain:         'https://inscriptions.parkourparis.fr/',
+      port:           Rails.application.credentials.mailgun[:smtp_port],
+      address:        Rails.application.credentials.mailgun[:smtp_server],
+      user_name:      Rails.application.credentials.mailgun[:smtp_login],
+      password:       Rails.application.credentials.mailgun[:smtp_password],
+      domain:         Rails.application.credentials.mailgun[:domain]
       authentication: :plain,
   }
   config.action_mailer.delivery_method = :smtp
