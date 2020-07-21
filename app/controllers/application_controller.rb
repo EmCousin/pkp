f80de21b436a711dccbb16c18df3033b1a4eba24
EmCousin <emmanuel@braidio.com>
2020-07-21 16:30:28 +0200

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,18 @@ class ApplicationController < ActionController::Base
 
   include DeviseOverrides
 
-  before_action :redirect_to_profile_completion_page!, unless: -> { devise_controller? || current_user.valid?(:account_setup) }
+  before_action :redirect_to_profile_completion_page!, if: :should_complete_profile?
 
   private
 
   def redirect_to_profile_completion_page!
     redirect_to edit_user_registration_path, notice: 'Veuillez compléter votre profil pour continuer'
+  end
+
+  def should_complete_profile?
+    return false if devise_controller?
+    return false unless user_signed_in?
+
+    !current_user.valid?(:account_setup)
   end
 end
