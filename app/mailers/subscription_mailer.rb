dee0f3901d12f65f17fa254c7f501a5a11821de8
EmCousin <emmanuel@braidio.com>
2020-07-30 22:23:22 +0200

    Update subscription_mailer.rb

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -11,19 +11,4 @@ class SubscriptionMailer < ApplicationMailer
 
     mail to: subscription.member.email, subject: "Inscription Parkour Paris #{subscription.year} / #{subscription.year + 1}"
   end
-
-  private
-
-  def pdf_from_subscription(subscription)
-    WickedPdf.new.pdf_from_string(
-      render_to_string(
-        'templates/subscription.html.erb',
-        layout: 'pdf.html.erb',
-        encoding: 'UTF-8',
-        locals: {
-          subscription: subscription
-        }
-      )
-    ).force_encoding('UTF-8')
-  end
 end
