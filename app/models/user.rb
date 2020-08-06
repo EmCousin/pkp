2c48f44345e365d3c61212b0dd9df5b86ea9fb8c
EmCousin <emmanuel@braidio.com>
2020-08-06 15:15:07 +0200

    remove confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable, :lockable, :timeoutable
+         :lockable, :timeoutable
 
   include DeviseExtensions::Validatable
 
@@ -15,7 +15,7 @@ class User < ApplicationRecord
 
   validates :terms_of_service, acceptance: true
 
-  with_options on: :account_setup, if: ->(user) { user.confirmed? && !user.confirmed_at_changed? } do |user|
+  with_options on: :account_setup, if: ->(_user) { true } do |user| # user.confirmed? && !user.confirmed_at_changed?
     user.validates :phone_number, presence: true, phone: true
     user.validates :address, presence: true
     user.validates :zip_code, presence: true
