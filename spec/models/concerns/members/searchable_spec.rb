75a86c4cf5898c442bc471d34149392c818abfd8
EmCousin <emmanuel@braidio.com>
2020-08-09 01:53:37 +0200

    Update searchable_spec.rb

--- spec/models/concerns/members/searchable_spec.rb
+++ spec/models/concerns/members/searchable_spec.rb
@@ -59,23 +59,3 @@ describe Members::Searchable, type: :model do
     end
   end
 end
-
-module Members
-  module Searchable
-    extend ActiveSupport::Concern
-
-    class_methods do
-      def search(query)
-        return all unless query.present?
-
-        joins(:user).where(
-          'LOWER(first_name) LIKE :search
-          OR LOWER(last_name) LIKE :search
-          OR LOWER(users.email) LIKE :search
-          OR users.phone_number LIKE :search',
-          search: "%#{query.downcase}%"
-        )
-      end
-    end
-  end
-end
