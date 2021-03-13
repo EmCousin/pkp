f9efb3e6ab587543bd85fa1aa2e521e695ae56d1
EmCousin <emmanuel@braidio.com>
2021-03-13 11:53:33 -0300

    upgrade rails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,7 @@ Rails.application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
+
   config.action_mailer.smtp_settings = {
       port:           Rails.application.credentials.mailgun[:smtp_port],
       address:        Rails.application.credentials.mailgun[:smtp_server],
