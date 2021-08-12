73a1fb6f40559b66b370bb118d11c4c443db731e
EmCousin <emmanuel@hey.com>
2021-08-12 18:59:12 +0200

    remove rails ujs and add stimulus controllers

---
+++ app/src/javascripts/controllers/select_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "stimulus"
+import select from "tail.select.js/js/tail.select-full"
+
+export default class SelectController extends Controller {
+  static values = {
+    locale: String,
+  }
+
+  connect() {
+    select(this.element, {
+      search: true,
+      multiSelectAll: true,
+      deselect: true,
+      locale: this.hasLocaleValue ? this.localeValue : 'fr',
+    });
+  }
+}
