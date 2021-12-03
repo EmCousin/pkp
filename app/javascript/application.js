b3356fb4b574b84110a845d4c54dfe1656f41cfd
Emmanuel Cousin <emmanuel@hey.com>
2021-12-03 18:45:23 -0300

    webpacker to esbuild

---
+++ app/javascript/application.js
@@ -0,0 +1,23 @@
+// Entry point for the build script in your package.json
+import "./src/jquery"
+import "./src/jquery-ui"
+import "bootstrap"
+import "@hotwired/turbo-rails"
+
+import * as ActionCable from '@rails/actioncable'
+window.Cable = ActionCable.createConsumer()
+
+import * as ActiveStorage from '@rails/activestorage'
+ActiveStorage.start()
+
+import "tail.select.js/js/tail.select-full"
+
+// import 'places.js'
+
+import "./controllers"
+
+document.addEventListener("turbo:load", function() {
+  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="dropdown"]').dropdown()
+})
+
