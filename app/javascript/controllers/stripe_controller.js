dfe12d9403bbe031eaa1a818b14a73ad6f7e03f7
Emmanuel Cousin <emmanuel@hey.com>
2023-06-29 10:40:52 +0200

    Bring back stripe payments

---
+++ app/javascript/controllers/stripe_controller.js
@@ -0,0 +1,69 @@
+import { Controller } from "@hotwired/stimulus"
+import { loadStripe } from '@stripe/stripe-js'
+
+export default class StripeController extends Controller {
+  static targets = ["form", "cardElement", "cardErrors"]
+  static values = { key: String }
+
+  async connect() {
+    const stripe = await loadStripe(this.keyValue)
+    const card = this.mountCard(stripe)
+
+    this.sendTokenToServerOnSubmit(stripe, card)
+  }
+
+  mountCard(stripe) {
+    const style = {
+      base: {
+        color: '#303238',
+        fontSize: '16px',
+        fontFamily: '"Open Sans", sans-serif',
+        fontSmoothing: 'antialiased',
+        '::placeholder': {
+          color: '#CFD7DF',
+        },
+      },
+      invalid: {
+        color: '#e5424d',
+        ':focus': {
+          color: '#303238',
+        },
+      },
+    }
+
+    const card = stripe.elements().create('card', {style})
+    card.mount(this.cardElementTarget)
+    card.addEventListener('change', (event) => {
+      this.displayError(event?.error?.message || '')
+    })
+
+    return card
+  }
+
+  sendTokenToServerOnSubmit(stripe, card) {
+    let form = this.formTarget
+    form.addEventListener('submit', async (event) => {
+      event.preventDefault()
+      let {error, token} = await stripe.createToken(card)
+      if (error) {
+        this.displayError(error.message)
+      } else {
+        this.insertTokenIntoFormAndSubmit(token)
+      }
+    })
+  }
+
+  displayError(errorMessage) {
+    this.cardErrorsTarget.textContent = errorMessage
+  }
+
+  insertTokenIntoFormAndSubmit(token) {
+    const form = this.formTarget
+    let hiddenInput = document.createElement('input')
+    hiddenInput.setAttribute('type', 'hidden')
+    hiddenInput.setAttribute('name', 'stripeToken')
+    hiddenInput.setAttribute('value', token.id)
+    form.appendChild(hiddenInput)
+    form.submit()
+  }
+}
