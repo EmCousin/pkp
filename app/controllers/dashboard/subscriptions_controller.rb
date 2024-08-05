8b08be47e8a3fb90a276cf616b01ee3f586edbe9
Emmanuel Cousin <emmanuel@hey.com>
2024-08-05 10:00:11 +0200

    brakeman

--- app/controllers/dashboard/subscriptions_controller.rb
+++ app/controllers/dashboard/subscriptions_controller.rb
@@ -42,7 +42,7 @@ module Dashboard
     end
 
     def set_member
-      @member = Member.find_by(id: params[:member_id])
+      @member = current_user.members.find_by(id: params[:member_id])
     end
   end
 end
