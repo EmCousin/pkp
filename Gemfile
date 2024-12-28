e77e25227623144f0c6d2c5d679f08e4adbc3658
Emmanuel Cousin <emmanuel@hey.com>
2024-12-28 13:18:21 -0300

    install tailwind and redesign Devise views

--- Gemfile
+++ Gemfile
@@ -47,6 +47,9 @@ gem 'stripe'
 gem 'wicked_pdf'
 gem 'wkhtmltopdf-binary'
 
+# Add this line to your Gemfile
+gem 'tailwindcss-rails'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
---
+++ config/tailwind.config.js
@@ -0,0 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './app/views/**/*.erb',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/components/**/*.{erb,html,rb}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
