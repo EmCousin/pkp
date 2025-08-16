af2b67153d9a98a1d0f1e9e3f66edeaae534fe99
Emmanuel Cousin <emmanuel@hey.com>
2025-08-16 22:05:58 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,26 @@
 Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
+
+  get '/wp-content/*path', to: ->(_env) { [429, {}, ['
+         / \
+        |\_/|
+        |---|
+        |   |
+        |   |
+      _ |=-=| _
+  _  / \|   |/ \
+ / \|   |   |   ||\
+|   |   |   |   | \>
+|   |   |   |   |   \
+| -   -   -   - |)   )
+|                   /
+ \                 /
+  \               /
+   \             /
+    \           /
+  ']] }
+
   get "up" => "rails/health#show", as: :rails_health_check
 
   devise_for :users, controllers: {
