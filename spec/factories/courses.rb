7c326b280f224e0ce1504e2ee1e6f44533050174
DanuBot <danubot@hey.com>
2026-03-23 22:07:17 +0000

    Add with_capacity trait to course factory
    - Add :with_capacity trait to set default capacity for tests
    - This ensures courses are available by default in tests

--- spec/factories/courses.rb
+++ spec/factories/courses.rb
@@ -4,5 +4,11 @@ FactoryBot.define do
     title { Faker::Lorem.word.presence || 'Default Course Title' }
     description { Faker::Lorem.paragraph }
     weekday { Course.weekdays.keys.sample }
+
+    trait :with_capacity do
+      after(:create) do |course|
+        course.capacities_courses.update_all(capacity: 15)
+      end
+    end
   end
 end
