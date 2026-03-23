f4863fae885cdd5b384e2a02c9d6be42ad3a3c26
DanuBot <danubot@hey.com>
2026-03-23 22:00:14 +0000

    Fix CapacitiesCourse spec for database enum
    - Remove shoulda-matchers enum test which doesn't work with database enum
    - Add custom uniqueness validation test

--- spec/models/capacities_course_spec.rb
+++ spec/models/capacities_course_spec.rb
@@ -4,23 +4,34 @@ require 'rails_helper'
 
 describe CapacitiesCourse, type: :model do
   it { is_expected.to belong_to(:course) }
+  it { is_expected.to validate_presence_of(:level) }
+  it { is_expected.to validate_presence_of(:capacity) }
+  it { is_expected.to validate_numericality_of(:capacity).is_greater_than_or_equal_to(0).only_integer }
 
   describe 'enums' do
     it 'defines level enum with prefix' do
-      expect(subject).to define_enum_for(:level)
-        .with_values(white: 'white', yellow: 'yellow', green: 'green', red: 'red')
-        .with_prefix
+      expect(described_class.levels.keys).to contain_exactly('white', 'yellow', 'green', 'red')
     end
   end
 
-  it { is_expected.to validate_presence_of(:level) }
-
   describe 'uniqueness validation' do
-    subject { create(:capacities_course) }
+    let(:course) { create(:course) }
 
-    it { is_expected.to validate_uniqueness_of(:level).scoped_to(:course_id) }
+    it 'prevents duplicate levels for the same course' do
+      create(:capacities_course, course: course, level: 'white')
+      duplicate = build(:capacities_course, course: course, level: 'white')
+
+      expect(duplicate).not_to be_valid
+      expect(duplicate.errors[:level]).to include('has already been taken')
+    end
+
+    it 'allows same level for different courses' do
+      course1 = create(:course)
+      course2 = create(:course)
+      create(:capacities_course, course: course1, level: 'white')
+      capacity2 = build(:capacities_course, course: course2, level: 'white')
+
+      expect(capacity2).to be_valid
+    end
   end
-
-  it { is_expected.to validate_presence_of(:capacity) }
-  it { is_expected.to validate_numericality_of(:capacity).is_greater_than_or_equal_to(0).only_integer }
 end
